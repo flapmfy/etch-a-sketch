4f0ac071e6c0c753d21057c87b401aee8530c2fe
František Kasík <frantis3k.kasik@gmail.com>
2023-03-19 11:30:27 +0100

    Refactor function names

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const canvas = document.querySelector(".js-canvas");
+const canvasDiv = document.querySelector(".js-canvas");
 const sizeSetter = document.querySelector(".hero__settings");
 const reset = document.querySelector(".js-reset");
 const start = document.querySelector(".js-start");
@@ -8,15 +8,15 @@ const start = document.querySelector(".js-start");
 
 function createBox(size) {
     const box = document.createElement("div");
-    box.style.cssText = `width: ${size}px; height: ${size}px; background-color: orange; border: 1px solid black`;
+    box.style.cssText = `width: ${size}px; height: ${size}px; background-color: white; border: 1px solid black`;
     return box;
 }
 
-function drawLine(pixelCount) {
+function drawLine(boxCount) {
     let line = document.createElement("div");
     line.style.cssText = "display: flex";
 
-    for (let i = 0; i < pixelCount; i++) {
+    for (let i = 0; i < boxCount; i++) {
         line.appendChild( createBox(50) );
     }
 
@@ -24,12 +24,12 @@ function drawLine(pixelCount) {
 }
 
 function drawCanvas(size) {
-    const canvasOut = document.createElement("div");
+    const canvasGrid = document.createElement("div");
     for (let i = 0; i < size; i++) {
-        canvasOut.appendChild(drawLine(size));
+        canvasGrid.appendChild(drawLine(size));
     }
 
-    canvas.appendChild(canvasOut);
+    canvasDiv.appendChild(canvasGrid);
 }
 
 drawCanvas(10);
